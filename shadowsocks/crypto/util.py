b45dff85b9b8a89b7019febc7758b20ba1c08d38
iinux <iinux@qq.com>
2025-12-28 20:34:54 +0800

    util

--- shadowsocks/crypto/util.py
+++ shadowsocks/crypto/util.py
@@ -77,7 +77,10 @@ def find_library(possible_lib_names, search_symbol, library_name,
         if os.name == "nt":
             paths.extend(find_library_nt(name))
         else:
-            path = ctypes.util.find_library(name)
+            try:
+                path = ctypes.util.find_library(name)
+            except FileNotFoundError as e:
+                pass
             if path:
                 paths.append(path)
 
